6cfac423b71e97f302222c9aa1292cfc6166e6fd
Isroil Omonjonov <isroiljonomonjonov@gmail.com>
2023-06-16 16:50:00 +0500

    App.vue changes and counter

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+<div class="container">
+    <h1>Counter: {{counter}}</h1>
+    <div class="d-flex">
+        <button @click="onInc" class="btn btn-success">ADD</button>
+        <button @click="onDec" class="btn btn-danger">Del</button>
+    </div>
+</div> 
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            counter: 0,
+        }
+    },
+    methods: {
+        onInc() {
+            this.counter += 1
+        },
+        onDec() {
+            this.counter -= 1
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
